4635715fc864af778d4b5a35371bf9c38f39d25f
申铎 张 <644954548@qq.com>
2018-11-09 11:19:44 +0800

    almost there, winner did not finished

--- gobang/Class1.cs
+++ gobang/Class1.cs
@@ -12,43 +12,39 @@ namespace gobang
         public Algorithm()
         {
 
-        }
-
-        public bool legal(int[][] black,int[][] white)
-        {
-            return true;
-        }
-
+        }//not finished
         public int winner()
         {
             return 1;
-        }
+        }//not finished
     }
 
     class ChessBoard
     {
-        public ChessBoard(Point vertex,double sizePerLine)
+        public ChessBoard(Point vertex,int sizePerLine)
         {
-            xBoundary = new double[20];
+            SizePerLine = sizePerLine;
+            xBoundary = new int[20];
             for (int i = 0; i < 20; i++)
             {
-                xBoundary[i] = vertex.X - sizePerLine / 2 + sizePerLine * i;
+                xBoundary[i] = vertex.X + sizePerLine * i;
             }
-            for (int i = 0; i < 20; i++)
+            for (int i = 0; i < 20; i++)//here the lenth is 20 to satisfy the chessput methods
             {
-                yBoundary[i] = vertex.Y - sizePerLine / 2 + sizePerLine * i;
+                yBoundary[i] = vertex.Y + sizePerLine * i;
             }
-        }
-        public double[] xBoundary { get; set; }
-        public double[] yBoundary { get; set; }
+        }//need the vertex on the top left of the board and size to initialize.
+        public int SizePerLine { get; set; }// distance between line
+        public int[] xBoundary { get; set; }// position of each vertical line
+        public int[] yBoundary { get; set; }//position of each horizon line
     }
 
     class Chess:ChessBoard
     {
-        public Chess(Point vertex,double sizePerLine):base(vertex,sizePerLine)
+        public Chess(Point vertex,int sizePerLine):base(vertex,sizePerLine)
         {
-            step = 0;
-            matrix = new int[19,19];
+            step = 0;//this is used in the recovering game.
+            matrix = new int[19,19];//size of matrix
             for (int i = 0; i < 19; i++)
             {
                 for (int j = 0; j < 19; j++)
@@ -56,9 +52,9 @@ namespace gobang
                     matrix[i, j] = -1;
                 }
             }//all content initialized to -1
-            point = new Point[181];
+            point = new Point[181];//initialize the point array
         }
-        public int step = new int();
+        public int step = new int();//start at 0 to note down the step untill now, used in the recovery.
         public Point[] point
         {
             get
@@ -69,23 +65,39 @@ namespace gobang
             {
                 this.point = value;
             }
-        }
-        public int[,] matrix { get; set; }//the content in this matrix show the order of the chess put, as well as the order of point.
+        }//store the cordinate center of cross that has a chess on it.
+        public int[,] matrix{ get; set; }//the content in this matrix show the order of the chess put, as well as the order of point.
         public void Chessput(Point xy)
         {
             for (int i = 0; i < 19; i++)
             {
                 for (int j = 0; j < 19; j++)
                 {
-                    if (xBoundary[j]<xy.X&xy.X<=yBoundary[j+1]&&yBoundary[i]<xy.Y&xy.Y<=yBoundary[i])
+                    if (
+                        (xBoundary[j]-SizePerLine)<xy.X&xy.X<=(xBoundary[j+1]-SizePerLine)
+                        &&
+                        (yBoundary[i]-SizePerLine)<xy.Y&xy.Y<=(yBoundary[i+1]-SizePerLine)
+                        )
                     {
                         matrix[i, j] = step;
-                        point[step] = xy;
+                        point[step] = new Point(xBoundary[j], yBoundary[i]);
                     }
                 }
             }
             step += 1;
-        }
+        }//fill the matrix and point array when put the chess in, do it after you do the checking. Have an imaginary boundary on the right and bottom
+        public bool Check(Point toput)
+        {
+            bool result = true;
+            foreach (Point item in point)
+            {
+                if (item.X-toput.X<=SizePerLine/2&item.Y-toput.Y<=SizePerLine)
+                {
+                    result = false;
+                }
+            }
+            return result;
+        }//use the distance to judge, need to run twice to check both white and black.
 
     }
 
